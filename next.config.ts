a5c4a2a26fd1371376204c6ce391ba5134ee69d7
asadawuth <122080707+asadawuth@users.noreply.github.com>
2025-07-03 13:22:35 +0700

    13.22

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,11 @@
 const nextConfig = {
-  compress: true,
-  swcMinify: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
+  compress: true,
+  swcMinify: true,
   experimental: {
     optimizeCss: true,
     legacyBrowsers: false,
   },
-  images: {
-    formats: ["image/webp", "image/avif"],
-  },
 };
-
-module.exports = nextConfig;
