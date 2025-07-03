031d88a80b077388ee553fba9a343fe6565ad5ec
asadawuth <122080707+asadawuth@users.noreply.github.com>
2025-07-03 13:16:00 +0700

    13.15

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,27 @@
-const nextConfig = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+const nextConfig = withBundleAnalyzer({
+  compress: true,
+  swcMinify: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
-  compress: true,
-  swcMinify: true,
   experimental: {
     optimizeCss: true,
     legacyBrowsers: false,
   },
-};
+  images: {
+    formats: ["image/webp", "image/avif"],
+  },
+  modularizeImports: {
+    lodash: {
+      transform: "lodash/{{member}}",
+    },
+    "date-fns": {
+      transform: "date-fns/{{member}}",
+    },
+  },
+});
+module.exports = nextConfig;
